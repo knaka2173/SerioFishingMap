49095c8b3f1e97550d4103704391ba215d1abd45
azarashiomp <kouichirouexample@icloud.com>
2025-04-20 16:27:10 +0900

    [FIX]APIリクエスト

--- Frontend/serio_fishingmap/src/app/fishing-spot-main-map/page.tsx
+++ Frontend/serio_fishingmap/src/app/fishing-spot-main-map/page.tsx
@@ -1,8 +1,29 @@
+"use client";
+import { useEffect, useState } from "react";
+
 export default function MapPage() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      await fetch("/api/testApi")
+        .then((response) => response.json())
+        .then((data) => setData(data))
+        .catch((error) => console.error("Error fetching data;", error));
+    }
+
+    fetchData();
+  }, []);
+
+  if (!data) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
       <h1>Map</h1>
       <p>This is the map page.</p>
+      {/* {data ? <p>{data.message}</p> : <p>Loading...</p>} */}
     </div>
   );
 }
