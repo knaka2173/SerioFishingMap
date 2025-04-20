d1bd49acafb2f0a181de2ff7ac91dc5fe241086a
azarashiomp <kouichirouexample@icloud.com>
2025-04-20 16:19:29 +0900

    [FIX]APIリクエスト

---
+++ Frontend/serio_fishingmap/src/app/api/testApi/route.ts
@@ -0,0 +1,20 @@
+//import { NextApiRequest, NextApiResponse } from "next";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  return NextResponse.json({
+    message: "データを取得!",
+  });
+}
+
+// export default function handler(req: NextApiRequest, res: NextApiResponse) {
+//     if (req.method === 'GET') {
+//         // GETリクエストの処理
+//         res.status(200).json({ message: 'GET request successful' });
+//     } else if (req.method === 'POST') {
+//         // POSTリクエストの処理
+//         res.status(200).json({ message: 'POST request successful' });
+//     } else {
+//         res.status(405).json({ message: 'Method not allowed' });
+//     }
+// }
