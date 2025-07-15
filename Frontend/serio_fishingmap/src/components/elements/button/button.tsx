404b258fe2c898d172d441b1b7d53582d48be0fd
azarashiomp <kouichirouexample@icloud.com>
2025-07-15 23:42:24 +0900

    MOD ボタンモードを追加

--- Frontend/serio_fishingmap/src/components/elements/button/button.tsx
+++ Frontend/serio_fishingmap/src/components/elements/button/button.tsx
@@ -1,32 +1,64 @@
-import { FC, ButtonHTMLAttributes } from "react";
-import classNames from "classnames";
-import styles from "./button.module.css";
+import { Button, ButtonProps } from "@chakra-ui/react";
+import { FC } from "react";
 
-interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  size?: "small" | "medium" | "large";
-  visual?: "primary" | "secondary" | "alert";
-}
+type CustomButtonProps = {
+  mode?: "primary" | "danger" | "secondary" | "ghost" | "success" | "warning";
+  label: string;
+  size?: "lg" | "md" | "sm" | "xs";
+  color?: string; //任意でオーバーライド
+  leftIcon?: string;
+  rightIcon?: string;
+  isLoading?: boolean;
+  loadingText?: string;
+  isDisabled?: boolean;
+} & ButtonProps;
 
-const Button: FC<ButtonProps> = ({
-  size = "medium",
-  visual = "primary",
-  children,
-  className,
-  ...props
-}) => {
-  return (
-    <button
-      className={classNames(
-        styles.button,
-        styles[size],
-        styles[visual],
-        className,
-      )}
-      {...props}
-    >
-      {children}
-    </button>
-  );
+const getModeStyles = (
+  mode: string
+): { colorScheme: string; variant?: string } => {
+  switch (mode) {
+    case "danger":
+      return { colorScheme: "red", variant: "solid" };
+    case "secondary":
+      return { colorScheme: "gray", variant: "solid" };
+    case "ghost":
+      return { colorScheme: "gray", variant: "ghost" };
+    case "success":
+      return { colorScheme: "green", variant: "solid" };
+    case "warning":
+      return { colorScheme: "yellow", variant: "solid" };
+    case "primary":
+    default:
+      return { colorScheme: "cyan", variant: "solid" };
+  }
 };
 
-export default Button;
+export const CustomButton: FC<CustomButtonProps> = ({
+  mode = "primary",
+  label,
+  size = "md",
+  color,
+  leftIcon,
+  rightIcon,
+  isLoading = false,
+  loadingText,
+  isDisabled = false,
+  ...props
+}) => {
+  const { colorScheme, variant } = getModeStyles(mode);
+
+  return (
+    <Button
+      spinnerPlacement="start"
+      isDisabled={isDisabled}
+      colorScheme={color || colorScheme}
+      variant={variant}
+      borderRadius="md"
+      px={6}
+      py={4}
+      {...props}
+    >
+      {label}
+    </Button>
+  );
+};
