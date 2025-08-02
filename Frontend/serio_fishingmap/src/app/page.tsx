9a38f55b46e6f6e30eb3c71768fadece3e960869
azarashiomp <kouichirouexample@icloud.com>
2025-08-03 01:54:16 +0900

    MOD home画面より画面遷移を実装

--- Frontend/serio_fishingmap/src/app/page.tsx
+++ Frontend/serio_fishingmap/src/app/page.tsx
@@ -1,11 +1,25 @@
+"use client";
 import { Sidebar } from "@/components/elements/Sidebar/sidebar";
-import { Heading, Text } from "@chakra-ui/react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+
+  const handleClickFishList = () => {
+    router.push("view-lists/fish-list");
+  };
+
   return (
-    <Sidebar>
-      <Heading>ホーム</Heading>
-      <Text>これはChakra UI v3で動作するサイドバー付きレイアウトです。</Text>
-    </Sidebar>
+    <span>
+      {/* 画面遷移の方法の標準については別途検討。ここではテストコードとして実装 */}
+      <button onClick={handleClickFishList}>魚一覧へ</button>
+
+      <Sidebar></Sidebar>
+      {/* TODO:Sidebarコンポーネントの修正
+      ・ヘッダーコンポーネントより開閉可能とする。
+        ・コンポーネント表示時はメインコンテンツと横並びにできることを確認する
+      ・アプリ名は不要
+      */}
+    </span>
   );
 }
--- Frontend/serio_fishingmap/src/app/view-lists/fish-list/page.tsx
+++ Frontend/serio_fishingmap/src/app/view-lists/fish-list/page.tsx
@@ -1,4 +1,24 @@
-export default function MapPage() {
+"use client";
+import { useEffect, useState } from "react";
+
+export default function FishListPage() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      await fetch("/api/testApi")
+        .then((response) => response.json())
+        .then((data) => setData(data))
+        .catch((error) => console.error("Error fetching data;", error));
+    }
+
+    fetchData();
+  }, []);
+
+  if (!data) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
       <h1></h1>
