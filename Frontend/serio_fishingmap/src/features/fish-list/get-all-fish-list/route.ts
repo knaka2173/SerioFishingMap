6db11488f4f7d3c6bc1f0833c6161c8412811751
kei-nakamura7 <kei_nakamura@serio.inc>
2025-10-22 09:50:34 +0900

    追加修正

--- Frontend/serio_fishingmap/src/features/fish-list/get-all-fish-list/route.ts
+++ Frontend/serio_fishingmap/src/features/fish-list/get-all-fish-list/route.ts
@@ -1,5 +1,3 @@
-// src/app/api/FishAllViews/route.ts
-
 import { fishRepository } from "@/features/common/repositories/fish.repository";
 
 export async function getAllFishList() {
--- Frontend/serio_fishingmap/src/features/fish-list/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function MapPage() {
-  return (
-    <div>
-      <h1></h1>
-      <p></p>
-    </div>
-  );
-}
