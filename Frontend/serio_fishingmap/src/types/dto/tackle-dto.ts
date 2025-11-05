fa77b990337a073c9a36d909a3732c59c8f0a87f
kei-nakamura7 <kei_nakamura@serio.inc>
2025-11-05 16:21:06 +0900

    各DBに登録しているEnumの型追加

---
+++ Frontend/serio_fishingmap/src/types/dto/tackle-dto.ts
@@ -0,0 +1,12 @@
+export enum FishingSpotType {
+  Shore = 1, // 岸（おかっぱり全般）
+  Breakwater = 2, // 堤防
+  Rock = 3, // 磯
+  Boat = 4, // 船
+  Kayak = 5, // カヤック
+  River = 6, // 河川
+  Lake = 7, // 湖・池
+  Surf = 8, // サーフ（砂浜）
+  Port = 9, // 港内
+  ManagedPond = 10, // 管理釣り場
+}
---
+++ Frontend/serio_fishingmap/src/types/enum/line-type.ts
@@ -0,0 +1,8 @@
+// ラインの種類
+export enum LineType {
+  Unknown = 0, // 不明
+  Nylon = 1,
+  Fluorocarbon = 2,
+  PE = 3,
+  Metal = 4,
+}
