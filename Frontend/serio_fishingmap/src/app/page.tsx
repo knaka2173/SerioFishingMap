1daa565c237eb2faf68b989eba275565b9866698
azarashiomp <kouichirouexample@icloud.com>
2025-12-14 21:35:16 +0900

    ページコンテンツがサイドバーに被らないよう修正

--- Frontend/serio_fishingmap/src/app/page.tsx
+++ Frontend/serio_fishingmap/src/app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { useRouter } from "next/navigation";
-import { Sidebar } from "@/components/elements/Sidebar/sidebar";
 
 export default function Home() {
   const router = useRouter();
@@ -10,16 +9,9 @@ export default function Home() {
   };
 
   return (
-    <span>
+    <div>
       {/* 画面遷移の方法の標準については別途検討。ここではテストコードとして実装 */}
       <button onClick={handleClickFishList}>魚一覧へ</button>
-
-      <Sidebar></Sidebar>
-      {/* TODO:Sidebarコンポーネントの修正
-      ・ヘッダーコンポーネントより開閉可能とする。
-        ・コンポーネント表示時はメインコンテンツと横並びにできることを確認する
-      ・アプリ名は不要
-      */}
-    </span>
+    </div>
   );
 }
