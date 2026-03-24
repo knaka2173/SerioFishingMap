93b1ee61ece3f6f26aff4628768f851a4d53b2d1
azarashiomp <kouichirouexample@icloud.com>
2026-03-24 22:47:29 +0900

    検索画面をコメントアウト

--- Frontend/serio_fishingmap/src/app/search/page.tsx
+++ Frontend/serio_fishingmap/src/app/search/page.tsx
@@ -1,28 +1,28 @@
-"use client";
-import { useEffect, useState } from "react";
+// "use client";
+// import { useEffect, useState } from "react";
 
-export default function MapPage() {
-  const [data, setData] = useState(null);
+// export default function MapPage() {
+//   const [data, setData] = useState(null);
 
-  useEffect(() => {
-    async function fetchData() {
-      await fetch("/api/testApi")
-        .then((response) => response.json())
-        .then((data) => { setData(data); })
-        .catch((error) => { console.error("Error fetching data;", error); });
-    }
+//   useEffect(() => {
+//     async function fetchData() {
+//       await fetch("/api/testApi")
+//         .then((response) => response.json())
+//         .then((data) => { setData(data); })
+//         .catch((error) => { console.error("Error fetching data;", error); });
+//     }
 
-    fetchData();
-  }, []);
+//     fetchData();
+//   }, []);
 
-  if (!data) {
-    return <div>Loading...</div>;
-  }
+//   if (!data) {
+//     return <div>Loading...</div>;
+//   }
 
-  return (
-    <div>
-      <h1></h1>
-      <p></p>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <h1></h1>
+//       <p></p>
+//     </div>
+//   );
+// }
