37539291d1b7edcf9933aaba2614553e90091fec
koichiro_honda <koichiro_honda@serio.inc>
2026-06-02 12:42:04 +0900

    6/2作業分

--- Frontend/serio_fishingmap/src/app/details/fish-details/page.tsx
+++ Frontend/serio_fishingmap/src/app/details/fish-details/page.tsx
@@ -1,12 +1,13 @@
 "use client";
-import Image from "@chakra-ui/react";
+import { Image } from "@chakra-ui/react";
+import { Label } from "@/components/elements/Label/Label";
 import { Fish } from "@/types/dto/fish-dto";
 
 const mockdata: Fish = {
   fishID: 1,
-  fishName: "カワハギ",
-  fishOrder: "test",
-  category: "ハギ",
+  fishName: "テストName",
+  fishOrder: "テストOrder",
+  category: "テストCategory",
 };
 
 const imageObjectUrl = "https://picsum.photos/300/200?random=1"; //テストデータ
@@ -16,6 +17,8 @@ export default function MapPage() {
 
   return (
     <div>
+      <Label text={fishdata.fishName} color="primary" size="title" />
+      <Label text={fishdata.category} color="secondary" size="subtitle" />
       <Image
         src={imageObjectUrl}
         alt={fishdata.fishName}
